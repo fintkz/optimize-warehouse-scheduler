77f22fe1bcbedfb112e4144b228548ddd7824104
Nilesh <fintkz@air.local>
2025-04-10 10:32:55 +0530

    fuck .env

--- src/config/apiUrl.ts
+++ src/config/apiUrl.ts
@@ -1,4 +1,5 @@
 // src/config/apiUrl.ts
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://web-production-713b0.up.railway.app/api/v1/schedule';
+const API_URL = 'https://web-production-713b0.up.railway.app/api/v1/schedule';
+
 export default API_URL;
